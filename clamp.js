d02c3a290d92c2a28321d2e32fd916c887927c34
Todd McNeal <todd@curalate.com>
2014-11-13 16:58:51 -0500

    Clamp -> Cramp

--- clamp.js
+++ clamp.js
@@ -1,261 +1,183 @@
 /*!
-* Clamp.js 0.5.1
+* Cramp.js 0.1.0
 *
 * Copyright 2011-2013, Joseph Schmitt http://joe.sh
 * Released under the WTFPL license
-* http://sam.zoy.org/wtfpl/
+* http://wtfpl.org
+*
+* Copyright (c) 2014 by Curalate, Inc.
+*
 */
 
-(function(){
-    /**
-     * Clamps a text node.
-     * @param {HTMLElement} element. Element containing the text node to clamp.
-     * @param {Object} options. Options to pass to the clamper.
-     */
-    function clamp(element, options) {
-        options = options || {};
+(function(window){
 
-        var self = this,
-            win = window,
-            opt = {
-                clamp:              options.clamp || 2,
-                useNativeClamp:     typeof(options.useNativeClamp) != 'undefined' ? options.useNativeClamp : true,
-                splitOnChars:       options.splitOnChars || ['.', '-', '–', '—', ' '], //Split on sentences (periods), hypens, en-dashes, em-dashes, and words (spaces).
-                animate:            options.animate || false,
-                truncationChar:     options.truncationChar || '…',
-                truncationHTML:     options.truncationHTML
-            },
-
-            sty = element.style,
-            originalText = element.innerHTML,
-
-            supportsNativeClamp = typeof(element.style.webkitLineClamp) != 'undefined',
-            clampValue = opt.clamp,
-            isCSSValue = clampValue.indexOf && (clampValue.indexOf('px') > -1 || clampValue.indexOf('em') > -1),
-            truncationHTMLContainer;
-            
-        if (opt.truncationHTML) {
-            truncationHTMLContainer = document.createElement('span');
-            truncationHTMLContainer.innerHTML = opt.truncationHTML;
-        }
-
+    var TEXT_NODE = 3,
+        DEFAULT_MAX_LINES = 2;
 
 // UTILITY FUNCTIONS __________________________________________________________
 
-        /**
-         * Return the current style for an element.
-         * @param {HTMLElement} elem The element to compute.
-         * @param {string} prop The style property.
-         * @returns {number}
-         */
-        function computeStyle(elem, prop) {
-            if (!win.getComputedStyle) {
-                win.getComputedStyle = function(el, pseudo) {
-                    this.el = el;
-                    this.getPropertyValue = function(prop) {
-                        var re = /(\-([a-z]){1})/g;
-                        if (prop == 'float') prop = 'styleFloat';
-                        if (re.test(prop)) {
-                            prop = prop.replace(re, function () {
-                                return arguments[2].toUpperCase();
-                            });
+    /**
+     * Return the current style for an element.
+     * @param {HTMLElement} element The element to compute.
+     * @param {string} prop The style property.
+     * @returns {number}
+     */
+    function computeStyle(element, prop) {
+        if (!window.getComputedStyle) {
+            window.getComputedStyle = function(el) {
+                this.el = el;
+                this.getPropertyValue = function(prop) {
+                    var re = /(\-([a-z]){1})/g;
+                    if (prop == 'float') prop = 'styleFloat';
+                    if (re.test(prop)) {
+                        prop = prop.replace(re, function () {
+                            return arguments[2].toUpperCase();
+                        });
+                    }
+                    return el.currentStyle && el.currentStyle[prop] ? el.currentStyle[prop] : null;
+                }
+                return this;
+            }
+        }
+
+        return window.getComputedStyle(element, null).getPropertyValue(prop);
+    };
+
+    /**
+     * Returns the maximum number of lines of text that should be rendered based
+     * on the current height of the element and the line-height of the text.
+     */
+    function getMaxLines(element, height) {
+        var availHeight = height || element.clientHeight,
+            lineHeight = getLineHeight(element);
+
+        return Math.max(Math.floor(availHeight/lineHeight), 0);
+    };
+
+    /**
+     * Returns the line-height of an element as an integer.
+     */
+    function getLineHeight(element) {
+        var lh = computeStyle(element, 'line-height');
+        if (lh == 'normal') {
+            // Normal line heights vary from browser to browser. The spec recommends
+            // a value between 1.0 and 1.2 of the font size. Using 1.1 to split the diff.
+            lh = parseInt(computeStyle(element, 'font-size')) * 1.2;
+        }
+        return parseInt(lh);
+    };
+
+    function getElementHeight(element) {
+        return element.clientHeight;
+    };
+
+    /**
+     * Returns the max number of lines we will allow before cramping.
+     */
+    function parseCrampValue(crampValue) {
+        var isCSSValue;
+
+        if (!crampValue) {
+            return DEFAULT_MAX_LINES;
+        }
+
+        isCSSValue = crampValue.indexOf && (crampValue.indexOf('px') > -1 || crampValue.indexOf('em') > -1);
+
+        if (crampValue == 'auto') {
+            return getMaxLines(element);
+        }
+        else if (isCSSValue) {
+            return getMaxLines(element, parseInt(crampValue));
+        } else {
+            return crampValue;
+        }
+    };
+
+    /**
+     * Cramps a text node.
+     * @param {HTMLElement} element. Element containing the text node to clamp.
+     */
+    function cramp(element, opts) {
+        var options = opts || {},
+            original = element.innerHTML,
+            cramped,
+            maxLines = parseCrampValue(options.cramp || 2),
+            truncationText = options.truncationChar || "…";
+
+        var truncate = function(maxLines) {
+            var i, text, parentNode, spaceIndex,
+                nodes = element.childNodes;
+
+            for (i = nodes.length - 1; i >= 0; i--) {
+                node = nodes[i],
+                parentNode = node.parentNode;
+
+                if (node.nodeType === TEXT_NODE) {
+                    text = node.nodeValue;
+                    while (text.length > 0) {
+
+                        spaceIndex = text.lastIndexOf(" ");
+
+                        if (spaceIndex != -1) {
+                            // Remove the last word
+                            text = text.slice(0, spaceIndex);
+                        } else {
+                            // Remove the last letter
+                            text = text.slice(0, text.length - 1);
                         }
-                        return el.currentStyle && el.currentStyle[prop] ? el.currentStyle[prop] : null;
+
+                        // Strip word/letter and add the "..." string
+                        node.nodeValue = text + truncationText;
+
+                        // Does the content fit?
+                        if (isCramped(maxLines)) {
+                            return element.innerHTML;
+                        } else {
+                            text = text.slice(0, text.length - truncationText.length); // Remove the "..." string
+                        }
                     }
-                    return this;
-                }
-            }
 
-            return win.getComputedStyle(elem, null).getPropertyValue(prop);
-        }
+                    // We've removed all the text and it's not cramped yet - remove the node itself
+                    parentNode.removeChild(node);
 
-        /**
-         * Returns the maximum number of lines of text that should be rendered based
-         * on the current height of the element and the line-height of the text.
-         */
-        function getMaxLines(height) {
-            var availHeight = height || element.clientHeight,
-                lineHeight = getLineHeight(element);
+                } else {
 
-            return Math.max(Math.floor(availHeight/lineHeight), 0);
-        }
+                    // Remove the last element
+                    parentNode.removeChild(node);
 
-        /**
-         * Returns the maximum height a given element should have based on the line-
-         * height of the text and the given clamp value.
-         */
-        function getMaxHeight(clmp) {
-            var lineHeight = getLineHeight(element);
-            return lineHeight * clmp;
-        }
+                    // Add the "..." string
+                    parentNode.appendChild(new Text(truncationText));
 
-        /**
-         * Returns the line-height of an element as an integer.
-         */
-        function getLineHeight(elem) {
-            var lh = computeStyle(elem, 'line-height');
-            if (lh == 'normal') {
-                // Normal line heights vary from browser to browser. The spec recommends
-                // a value between 1.0 and 1.2 of the font size. Using 1.1 to split the diff.
-                lh = parseInt(computeStyle(elem, 'font-size')) * 1.2;
-            }
-            return parseInt(lh);
-        }
-
-
-// MEAT AND POTATOES (MMMM, POTATOES...) ______________________________________
-        var splitOnChars = opt.splitOnChars.slice(0),
-            splitChar = splitOnChars[0],
-            chunks,
-            lastChunk;
-        
-        /**
-         * Gets an element's last child. That may be another node or a node's contents.
-         */
-        function getLastChild(elem) {
-            //Current element has children, need to go deeper and get last child as a text node
-            if (elem.lastChild.children && elem.lastChild.children.length > 0) {
-                return getLastChild(Array.prototype.slice.call(elem.children).pop());
-            }
-            //This is the absolute last child, a text node, but something's wrong with it. Remove it and keep trying
-            else if (!elem.lastChild || !elem.lastChild.nodeValue || elem.lastChild.nodeValue == '' || elem.lastChild.nodeValue == opt.truncationChar) {
-                elem.lastChild.parentNode.removeChild(elem.lastChild);
-                return getLastChild(element);
-            }
-            //This is the last child we want, return it
-            else {
-                return elem.lastChild;
-            }
-        }
-        
-        /**
-         * Removes one character at a time from the text until its width or
-         * height is beneath the passed-in max param.
-         */
-        function truncate(target, maxHeight) {
-            if (!maxHeight) {return;}
-            
-            /**
-             * Resets global variables.
-             */
-            function reset() {
-                splitOnChars = opt.splitOnChars.slice(0);
-                splitChar = splitOnChars[0];
-                chunks = null;
-                lastChunk = null;
-            }
-            
-            var nodeValue = target.nodeValue.replace(opt.truncationChar, '');
-            
-            //Grab the next chunks
-            if (!chunks) {
-                //If there are more characters to try, grab the next one
-                if (splitOnChars.length > 0) {
-                    splitChar = splitOnChars.shift();
-                }
-                //No characters to chunk by. Go character-by-character
-                else {
-                    splitChar = '';
-                }
-                
-                chunks = nodeValue.split(splitChar);
-            }
-            
-            //If there are chunks left to remove, remove the last one and see if
-            // the nodeValue fits.
-            if (chunks.length > 1) {
-                // console.log('chunks', chunks);
-                lastChunk = chunks.pop();
-                // console.log('lastChunk', lastChunk);
-                applyEllipsis(target, chunks.join(splitChar));
-            }
-            //No more chunks can be removed using this character
-            else {
-                chunks = null;
-            }
-            
-            //Insert the custom HTML before the truncation character
-            if (truncationHTMLContainer) {
-                target.nodeValue = target.nodeValue.replace(opt.truncationChar, '');
-                element.innerHTML = target.nodeValue + ' ' + truncationHTMLContainer.innerHTML + opt.truncationChar;
-            }
-
-            //Search produced valid chunks
-            if (chunks) {
-                //It fits
-                if (element.clientHeight <= maxHeight) {
-                    //There's still more characters to try splitting on, not quite done yet
-                    if (splitOnChars.length >= 0 && splitChar != '') {
-                        applyEllipsis(target, chunks.join(splitChar) + splitChar + lastChunk);
-                        chunks = null;
-                    }
-                    //Finished!
-                    else {
+                    // Does the content fit?
+                    if (isCramped(maxLines)) {
                         return element.innerHTML;
+                    } else {
+                        parentNode.removeChild(parentNode.lastChild); // Remove the "..." string
                     }
                 }
             }
-            //No valid chunks produced
-            else {
-                //No valid chunks even when splitting by letter, time to move
-                //on to the next node
-                if (splitChar == '') {
-                    applyEllipsis(target, '');
-                    target = getLastChild(element);
-                    
-                    reset();
-                }
-            }
-            
-            //If you get here it means still too big, let's keep truncating
-            if (opt.animate) {
-                setTimeout(function() {
-                    truncate(target, maxHeight);
-                }, opt.animate === true ? 10 : opt.animate);
-            }
-            else {
-                return truncate(target, maxHeight);
-            }
-        }
-        
-        function applyEllipsis(elem, str) {
-            elem.nodeValue = str + opt.truncationChar;
-        }
 
+            return element.innerHTML;
+        };
+
+        var isCramped = function(lines) {
+            return getLineHeight(element) * lines >= getElementHeight(element);
+        };
 
 // CONSTRUCTOR ________________________________________________________________
 
-        if (clampValue == 'auto') {
-            clampValue = getMaxLines();
-        }
-        else if (isCSSValue) {
-            clampValue = getMaxLines(parseInt(clampValue));
+        if (isCramped(maxLines)) {
+            cramped = null;
+        } else {
+            cramped = truncate(maxLines);
         }
 
-        var clampedText;
-        if (supportsNativeClamp && opt.useNativeClamp) {
-            sty.overflow = 'hidden';
-            sty.textOverflow = 'ellipsis';
-            sty.webkitBoxOrient = 'vertical';
-            sty.display = '-webkit-box';
-            sty.webkitLineClamp = clampValue;
-
-            if (isCSSValue) {
-                sty.height = opt.clamp + 'px';
-            }
-        }
-        else {
-            var height = getMaxHeight(clampValue);
-            if (height <= element.clientHeight) {
-                clampedText = truncate(getLastChild(element), height);
-            }
-        }
-        
         return {
-            'original': originalText,
-            'clamped': clampedText
-        }
+            'original': original,
+            'cramped': cramped
+        };
     }
 
-    window.$clamp = clamp;
-})();
+    window.$cramp = cramp;
+
+})(window);
